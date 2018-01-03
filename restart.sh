62c9b080a7200585c17ea8b508e0f12a856a753a
Ruslan A <ruslan.absalyamov@gmail.com>
2018-01-03 15:00:06 +0300

    Fixed restart.sh

--- restart.sh
+++ restart.sh
@@ -4,7 +4,7 @@ sdir="$(dirname $0)"
 
 echo "Script dir=$sdir"
 
-echo "Killing all started mavenzip instances..."
+echo "Killing all started rsslog instances..."
 pgrep -a -f rsslog | awk '{print $1;}' | while read -r a; do kill -9 $a; done
 
 cd $sdir/dist
